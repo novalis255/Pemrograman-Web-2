f7f661239d9166f99cc8da9036a419fdd6f34e81
Novalis Ramadhan Syah <135153580+novalis255@users.noreply.github.com>
2024-03-05 20:32:11 +0700

    Add files via upload

---
+++ latihan-01(02).php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kesan Pertama Belajar PHP</title>
+</head>
+<body>
+    <?php
+    // Menampilkan kesan pertama saat belajar PHP
+    echo "<h1>Kesan Pertama Saat Belajar PHP</h1>";
+    echo "<p>Saat pertama kali belajar PHP, saya kurang tertarik dengan PHP tetapi semakin lama saya semakin tertarik karna dapat mmebuat halaman web yang dinamis dan interaktif. Dengan itu saya merasa antusias untuk belajar lebih lanjut.</p>";
+    ?>
+</body>
+</html>
---
+++ latihan-01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Latihan Pertemuan 01 </title>
+</head>
+<body>
+    <h3><center> Tugas Pertemuan 1 </center></h3>
+    <?php
+    print ("<center>");
+    print("<font face='arial' size=18 color='red'><center> BIODATA </center></font><br>");
+    print("<font face='arial' size=5 color='blue'> Nama\t\t :<b> Achmad Riki </b></font><br>");
+    print("<font face='arial' size=5 color='blue'> Email\t\t : ariki@yahoo.com </font><br>");
+    print("<font face='arial' size=5 color='blue'> Jurusan\t\t : Teknik Informatika </font><br>");
+    print("<font face='arial' size=5 color='blue'> Hobi\t\t : Travelling </font><br></center>");
+
+    ?>
+</body>
+</html>
